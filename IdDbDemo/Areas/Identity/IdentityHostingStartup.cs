4765f3fa262bde03aee9f5a5f0902e3068c507ed
David Karlsson <david.karlsson1@learnet.se>
2020-02-10 13:24:47 +0100

    Add project files.

---
+++ IdDbDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IdDbDemo.Data;
+using IdDbDemo.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdDbDemo.Areas.Identity.IdentityHostingStartup))]
+namespace IdDbDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ IdDbDemo/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace IdDbDemo.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+       
+        [Required]
+        public string LastName { get; set; }
+
+    }
+}
